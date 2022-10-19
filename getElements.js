088f580c56970a6aa93a34e98f8dd76774702350
Franco Flores <francobarbosaflores@gmail.com>
2022-10-19 17:54:50 -0300

    :tada: getElements and querySelector

---
+++ getElements.js
@@ -0,0 +1,27 @@
+//getElementsByID
+const element = document.getElementById('titulo') 
+console.log(element)
+
+
+//getElementsByClassName
+
+const elementTwo = document.getElementsByClassName('subtitulo')
+console.log(elementTwo)
+
+//getElementByTagName
+
+const elementThree = document.getElementsByTagName('meta')
+console.log(elementThree)
+
+//querySelector    get the first.
+const elementFour = document.querySelector('.subtitulo')
+console.log(elementFour)
+
+
+//querySelectorAll  get a Node List elements
+
+const elements = document.querySelectorAll('.subtitulo')
+console.log(elements)
+
+
+
